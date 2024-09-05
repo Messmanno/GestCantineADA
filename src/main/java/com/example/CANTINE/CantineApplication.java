bf73bb778a9cf6cb7a0c7a33d700f71f8ecfc5a2
Messmanno <mesmercoulibaly2002@gmail.com>
2024-09-05 13:42:44 +0000

    first commit

---
+++ src/main/java/com/example/CANTINE/CantineApplication.java
@@ -0,0 +1,13 @@
+package com.example.CANTINE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CantineApplication.class, args);
+	}
+
+}
